f819f0508613dfa74da044a5b64ec0310fa09007
Yelkcubwerdna <yelkcubwerdna@yahoo.com>
2025-12-20 15:04:07 -0500

    Adding upgrade mechanic-unfinished

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+from upgrade import Upgrade
+
 # main.py
 def main():
     coins = 0
+    enter = 1
+
+    upgrades = [
+        Upgrade("Enter+1", "Increases coins gained from pressing Enter", 50, "enter"),
+        Upgrade("Enter+2", "Increases coins gained from pressing Enter", 2500, "enter"),
+        Upgrade("Enter+3", "Increases coins gained from pressing Enter", 125000, "enter"),
+        Upgrade("Enter+4", "Increases coins gained from pressing Enter", 6250000, "enter"),
+        Upgrade("Enter+5", "Increases coins gained from pressing Enter", 312500000, "enter")
+    ]
     
     input("Press enter to get coins!")
-    coins+=1
 
     while(True):
+        coins+=enter
+        available = []
+        for i in range(0, len(upgrades)):
+            upgr = upgrades[i]
+            if upgr.cost <= coins:
+                available.append([i, upgr])
+                print(f'{len(available)} - {upgr.name} | {upgr.cost} coins | {upgr.desc}')
         print(f'Coins: {coins}')
 
-        input()
-        coins+=1
+        inp = input()
+        
+        if (inp != ""):
+            try:
+                # Implement Upgrade Stucture
+
 
 
 main()
---
+++ upgrade.py
@@ -0,0 +1,6 @@
+class Upgrade:
+    def __init__(self, name, desc, cost, func):
+        self.name = name
+        self.desc = desc
+        self.cost = cost
+        self.func = func
